c282a172c2ad7ad9f9870a30fa4d763626770c94
Naruf <102542599+Naruf@users.noreply.github.com>
2026-06-17 11:55:44 +0200

    Select functionality added

--- src/index.js
+++ src/index.js
@@ -1,23 +1,23 @@
 function updateDateTime() {
-  let mainClockElement = document.querySelector("#clock");
   let malagaCityTimeZone = moment.tz("Europe/Madrid");
-  let currentTime = malagaCityTimeZone.format("h:mm:ss [<small>]A[</small>]");
-  mainClockElement.innerHTML = currentTime;
-
+  //main section date
   let mainDateElement = document.querySelector("#main-date");
   let currentDate = malagaCityTimeZone.format("dddd, MMMM Do YYYY");
   mainDateElement.innerHTML = currentDate;
-
+  //Main section clock
+  let mainClockElement = document.querySelector("#clock");
+  let currentTime = malagaCityTimeZone.format("h:mm:ss [<small>]A[</small>]");
+  mainClockElement.innerHTML = currentTime;
+  //New York section
   let newYorkDateElement = document.querySelector("#new-york-date");
   let newYorkTimeElement = document.querySelector("#new-york-time");
   let newYorkCityTimeZone = moment.tz("America/New_York");
-
   newYorkDateElement.innerHTML =
     newYorkCityTimeZone.format("dddd, MMMM Do YYYY");
   newYorkTimeElement.innerHTML = newYorkCityTimeZone.format(
     "h:mm:ss [<small>]A[</small>]",
   );
-
+  //Paris section
   let parisDateElement = document.querySelector("#paris-date");
   let parisTimeElement = document.querySelector("#paris-time");
   let parisCityTimeZone = moment.tz("Europe/Paris");
@@ -25,7 +25,7 @@ function updateDateTime() {
   parisTimeElement.innerHTML = parisCityTimeZone.format(
     "h:mm:ss [<small>]A[</small>]",
   );
-
+  //Qatar section
   let qatarDateElement = document.querySelector("#qatar-date");
   let qatarTimeElement = document.querySelector("#qatar-time");
   let qatarCityTimeZone = moment.tz("Asia/Qatar");
@@ -33,6 +33,7 @@ function updateDateTime() {
   qatarTimeElement.innerHTML = qatarCityTimeZone.format(
     "h:mm:ss [<small>]A[</small>]",
   );
+  //Tokyo section
   let tokyoDateElement = document.querySelector("#tokyo-date");
   let tokyoTimeElement = document.querySelector("#tokyo-time");
   let tokyoCityTimeZone = moment.tz("Asia/Tokyo");
@@ -40,31 +41,35 @@ function updateDateTime() {
   tokyoTimeElement.innerHTML = tokyoCityTimeZone.format(
     "h:mm:ss [<small>]A[</small>]",
   );
+  //Hours difference function
   function hoursDiff() {
     let newYorkOffset = moment.tz("America/New_York").utcOffset();
     let parisOffSet = moment.tz("Europe/Paris").utcOffset();
     let qatarOffSet = moment.tz("Asia/Qatar").utcOffset();
     let tokyoOffSet = moment.tz("Asia/Tokyo").utcOffset();
     let malagaOffset = moment.tz("Europe/Madrid").utcOffset();
-
+    //NY hours diff
     let newYorkDiffMinutes = newYorkOffset - malagaOffset;
     let newYorkDiffHours = Math.floor(Math.abs(newYorkDiffMinutes) / 60);
     let sign = newYorkDiffMinutes >= 0 ? "+" : "-";
     let newYorkHoursDiffElement = document.querySelector("#new-york-diff");
     newYorkHoursDiffElement.innerHTML = `(${sign}${newYorkDiffHours} hours)`;
 
+    //Paris hours diff
     let parisDiffMinutes = parisOffSet - malagaOffset;
     let parisDiffHours = Math.floor(Math.abs(parisDiffMinutes) / 60);
     let parissign = parisDiffMinutes >= 0 ? "+" : "-";
     let parisHoursDiffElement = document.querySelector("#paris-diff");
     parisHoursDiffElement.innerHTML = `(${parissign}${parisDiffHours} hours)`;
 
+    //Qatar hours diff
     let qatarDiffMinutes = qatarOffSet - malagaOffset;
     let qatarDiffHours = Math.floor(Math.abs(qatarDiffMinutes) / 60);
     let qatarsign = qatarDiffMinutes >= 0 ? "+" : "-";
     let qatarHoursDiffElement = document.querySelector("#qatar-diff");
     qatarHoursDiffElement.innerHTML = `(${qatarsign}${qatarDiffHours} hours)`;
 
+    //Tokyo hours diff
     let tokyoDiffMinutes = tokyoOffSet - malagaOffset;
     let tokyoDiffHours = Math.floor(Math.abs(tokyoDiffMinutes) / 60);
     let tokyosign = tokyoDiffMinutes >= 0 ? "+" : "-";
@@ -73,5 +78,29 @@ function updateDateTime() {
   }
   hoursDiff();
 }
+//Select functionality and main location data replacement
+function updateMainLocation(event) {
+  let cityUpdatedTimeZone = event.target.value;
+  let cityNameUpdated = cityUpdatedTimeZone.split("/")[1];
+  let cityTimeUpdated = moment
+    .tz(cityUpdatedTimeZone)
+    .format("h:mm:ss [<small>]A[</small>]");
+  let cityDateUpdated = moment
+    .tz(cityUpdatedTimeZone)
+    .format("dddd, MMMM Do YYYY");
+  let updatedLocationElement = document.querySelector("#updated-main-location");
+  updatedLocationElement.innerHTML = `
+      <h2>${cityNameUpdated}</h2>
+      <p>
+        <div class="date" id="main-date">${cityDateUpdated}</div>
+        <div class="clock" id="clock">${cityTimeUpdated}</div>
+      </p>`;
+  let citiesHidden = document.querySelector("#cities-block");
+  citiesHidden.classList.add("hide");
+}
+
 updateDateTime();
 setInterval(updateDateTime, 1000);
+
+let updatedCity = document.querySelector("#city-select");
+updatedCity.addEventListener("change", updateMainLocation);
